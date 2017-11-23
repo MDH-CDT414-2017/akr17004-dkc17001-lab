18b729d8b2c125ee9ff78b97e48d1cca4b4b003a
Djordje Kalezic <djordjekalezic@61.90.243.130-v831.wireless.mdh.se>
2017-11-23 16:26:00 +0200

    Scores

--- BowlingGame/src/BowlingGameTest.java
+++ BowlingGame/src/BowlingGameTest.java
@@ -4,6 +4,8 @@
  * @version 1.0 
  * @date 2016-11-24
  */
+import org.junit.Before;
+
 import junit.framework.TestCase;
 
 /** BowlingGame Score calculator test cases 
@@ -14,12 +16,32 @@ public class BowlingGameTest extends TestCase {
 	/** test01 
 	 * 	
 	 *  If no game is provided, score should be -1 (error)   
-	 */	     
-	public void test01() {
-        BowlingGame bowlingGame = new BowlingGame("");
-        assertEquals(-1, bowlingGame.getScore());
+	 */	    
+
+	
+	public void testOpenScore() {
+        BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(81, bowlingGame.getScore());
     }	
 	
+
 	//TODO: Add more test cases below...
+	
+	public void testOpen() {
+        BowlingGame bowlingGame = new BowlingGame("");
+        assertEquals(true, bowlingGame.open( "1,3" ));
+    }	
+	public void testStrike() {
+        BowlingGame bowlingGame = new BowlingGame("");
+        assertEquals(true, bowlingGame.strike( "10,0" ));
+    }	
+	public void testSpare() {
+        BowlingGame bowlingGame = new BowlingGame("");
+        assertEquals(true, bowlingGame.spare( "8,2" ));
+    }	
+	
+	
+	
+	
 	
 }
